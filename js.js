470cb14e6829603e0433088011d3dd9edb273b23
niilliiz <niloo.vatan.nv@gmail.com>
2021-12-27 07:33:10 +0330

    dayFour: count fixed, star style fixed

--- js.js
+++ js.js
@@ -17,22 +17,35 @@ btn1.addEventListener("click", (e) => {
 
 /***********DAY FOUR***********************/
 
-const btn = document.querySelectorAll("button");
-console.dir(btn);
-
 const rate = document.getElementsByClassName("rate")[0];
 const btn4 = document.getElementsByClassName("btn4")[0];
 const star = document.getElementsByClassName("star")[0];
+const span = document.getElementsByClassName("fa-star")[0];
 
 const countText = document.getElementsByClassName("count")[0];
 
-const count = 27;
+let count = 27;
 
 countText.textContent = `${count}`;
 console.dir(star.classList);
 
-btn4.addEventListener("click", () => {
-    star.classList.remove("hoverStar");
+btn4.addEventListener("click", flyingStar);
+
+function flyingStar() {
+    // star.classList.remove("hoverStar");
+    span.style.color = "#ffd700";
     star.classList.add("flyingStar");
-    console.dir(star.classList);
-});
+}
+
+btn4.addEventListener("click", addCount);
+
+function addCount() {
+    setTimeout(() => {
+        count++;
+
+        countText.textContent = count;
+        countText.style.color = "#ffd700";
+    }, 600);
+    btn4.removeEventListener("click", addCount);
+    btn4.removeEventListener("click", flyingStar);
+}
