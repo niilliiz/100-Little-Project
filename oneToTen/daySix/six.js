1f00584f06af243e21390cbe2972ab86e342bfa0
niilliiz <niloo.vatan.nv@gmail.com>
2022-01-09 23:21:37 +0330

    complete the functionality

--- oneToTen/daySix/six.js
+++ oneToTen/daySix/six.js
@@ -5,9 +5,6 @@ const turn = document.querySelector(".turn");
 const result = document.querySelector(".result");
 const reset = document.querySelector(".reset");
 
-let currPlayer = "X";
-let count = 0;
-
 const winOrder = [
     [0, 1, 2],
     [3, 4, 5],
@@ -22,95 +19,132 @@ const winOrder = [
 document.addEventListener("DOMContentLoaded", createBoard);
 
 function createBoard() {
-    count = 0;
-    currPlayer = "X";
+    let won = false;
+    let count = 0;
+    let currPlayer;
 
+    // console.log(currPlayer);
+
+    result.textContent = "";
     result.style.display = "none";
     turn.style.display = "block";
-    turn.textContent = `${currPlayer} Turn`;
+    turn.textContent = `First Choose X|O`;
 
     for (let i = 0; i < boxes.length; i++) {
-        console.log(boxes[i].classList);
+        // console.log(boxes[i].classList);
         boxes[i].textContent = "";
-        boxes[i].classList.remove("X");
-        boxes[i].classList.remove("O");
-        boxes[i].addEventListener("click", () => clickBoxHandler.call(boxes[i]));
+        boxes[i].classList.remove("full", "X", "O", "won");
+        boxes[i].addEventListener("click", clickBoxHandler);
     }
-}
 
-function clickBoxHandler() {
-    this.textContent = currPlayer;
-    this.classList.add(currPlayer);
-    handleNextTurn();
-}
+    function clickBoxHandler() {
+        const currentBox = this;
 
-// function removeListenerHandler(box) {
-//     box.removeEventListener("click", clickBoxHandler, true);
-// }
+        if (!this.classList.contains("full") && currPlayer) {
+            // console.log(currPlayer);
+            currentBox.textContent = currPlayer;
+            currentBox.classList.add("full", currPlayer);
 
-function handleNextTurn() {
-    count++;
-    if (count <= 4) {
-        setNextPlayerHandler();
-    } else if (count > 4 && count < 9) {
-        winCheckingHandler();
-        if (count !== 0) {
-            setNextPlayerHandler();
+            handleNextTurn(currentBox);
         }
     }
-}
 
-function setNextPlayerHandler() {
-    if (currPlayer === "X") {
-        currPlayer = "O";
-        turn.textContent = `${currPlayer} Turn`;
-    } else {
+    function handleNextTurn(currBox) {
+        currBox.removeEventListener("click", clickBoxHandler);
+
+        // console.log(currBox);
+
+        count++;
+        setNextPlayerHandler();
+
+        if (count > 4) {
+            winCheckingHandler();
+        }
+    }
+
+    function setNextPlayerHandler() {
+        if (currPlayer === "X") {
+            currPlayer = "O";
+            turn.textContent = `${currPlayer} Turn`;
+        } else if (currPlayer === "O") {
+            currPlayer = "X";
+            turn.textContent = `${currPlayer} Turn`;
+        }
+    }
+
+    function winCheckingHandler() {
+        for (let i = 0; i < winOrder.length; i++) {
+            let b1 = boxes[winOrder[i][0]];
+            let b2 = boxes[winOrder[i][1]];
+            let b3 = boxes[winOrder[i][2]];
+            if (
+                b1.classList.contains("X") &&
+                b2.classList.contains("X") &&
+                b3.classList.contains("X")
+            ) {
+                won = true;
+
+                result.style.display = "block";
+                turn.style.display = "none";
+                result.textContent = "X won";
+
+                b1.classList.add("won");
+                b2.classList.add("won");
+                b3.classList.add("won");
+
+                clearBoardHandler();
+            }
+            if (
+                b1.classList.contains("O") &&
+                b2.classList.contains("O") &&
+                b3.classList.contains("O")
+            ) {
+                won = true;
+
+                result.style.display = "block";
+                turn.style.display = "none";
+                result.textContent = "O won";
+
+                b1.classList.add("won");
+                b2.classList.add("won");
+                b3.classList.add("won");
+
+                clearBoardHandler();
+            }
+            if (!won && count > 8) {
+                result.style.display = "block";
+                turn.style.display = "none";
+                result.textContent = "XO draw";
+            }
+        }
+    }
+
+    function clearBoardHandler() {
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i].removeEventListener("click", clickBoxHandler);
+        }
+    }
+
+    reset.addEventListener("click", createBoard);
+
+    xBtn.addEventListener("click", () => {
         currPlayer = "X";
         turn.textContent = `${currPlayer} Turn`;
-    }
+    });
+    oBtn.addEventListener("click", () => {
+        currPlayer = "O";
+        turn.textContent = `${currPlayer} Turn`;
+    });
 }
-
-function winCheckingHandler() {
-    for (let i = 0; i < winOrder.length; i++) {
-        let b1 = boxes[winOrder[i][0]];
-        let b2 = boxes[winOrder[i][1]];
-        let b3 = boxes[winOrder[i][2]];
-        if (
-            b1.classList.contains("X") &&
-            b2.classList.contains("X") &&
-            b3.classList.contains("X")
-        ) {
-            result.style.display = "block";
-            turn.style.display = "none";
-
-            result.textContent = "X won";
-            count = 0;
-            return;
-        }
-        if (
-            b1.classList.contains("O") &&
-            b2.classList.contains("O") &&
-            b3.classList.contains("O")
-        ) {
-            result.style.display = "block";
-            turn.style.display = "none";
-            result.textContent = "O won";
-            count = 0;
-            return;
-        }
-    }
-}
-
-reset.addEventListener("click", createBoard);
 
 /**
  * /TODO:
- * 1- style: turn
+ * 1- style: turn and result<================
  * 2- reset btn -> doesn't work correctly
  * 3- divide the code to different function
- * 4- what happens after someOne won -> 1- must finish the game with animate and reset the board after it.
+ * 4- what happens after someOne won -> 1- must finish the game with animate and reset the board after it.<=========================
  * 5- player 1 , 2
- * 6- after clicked n each item, disable them
- * 7- win or loose animation
+ * 6- after clicked on each item, disable them
+ * 7- win or loose animation <=====================
  * 8- u can replace the turn text with result like the game
  */
