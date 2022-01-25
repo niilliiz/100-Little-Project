803ec4f0c74bae304db38602d4041620a087809b
niilliiz <niloo.vatan.nv@gmail.com>
2022-01-25 07:59:06 +0330

    ch8: add times to clock

--- oneToTen/ch8/index.js
+++ oneToTen/ch8/index.js
@@ -1,41 +1,46 @@
-const theta = [];
+let theta = [];
 const times = [3, 2, 1, 12, 11, 10, 9, 8, 7, 6, 5, 4];
 
-function generateTheta(n, r, element) {
+function generateTheta(n) {
     let frag = 360 / n;
     for (let i = 0; i <= n; i++) {
+        console.log(theta);
         theta.push((frag / 180) * i * Math.PI);
     }
-    setUpClock(n, r, element);
 }
 
-function setUpClock(n, r, element) {
+function setUpCircle(n, r, element, type) {
     const clock = document.querySelector(element);
     const circleHeight = window.getComputedStyle(clock).height.slice(0, -2);
     for (let i = 0; i < n; i++) {
-        let dot = document.createElement("div");
-        dot.classList.add("dot");
+        let div = document.createElement("div");
+        div.classList.add(type);
 
-        if (i % 5) {
-            //smallDot
-            dot.classList.add("smallDot");
-        } else {
-            //big dot
-            dot.classList.add("bigDot");
-            // if ()
-            const span = document.createElement("span");
-            span.classList.add("time");
-            span.textContent = times[i / 5];
-            dot.appendChild(span);
+        if (type === "dot") {
+            if (i % 5) {
+                //smallDot
+                div.classList.add("smallDot");
+            } else {
+                //bigDot
+                div.classList.add("bigDot");
+            }
+        } else if (type === "time") {
+            console.log(n);
+            div.textContent = times[i];
         }
 
-        dot.posX = Math.round(r * Math.cos(theta[i])) + "px";
-        dot.posY = Math.round(r * Math.sin(theta[i])) + "px";
-        dot.style.top = circleHeight / 2 - parseInt(dot.posY.slice(0, -2)) + "px";
-        dot.style.left = circleHeight / 2 + parseInt(dot.posX.slice(0, -2)) + "px";
+        div.posX = Math.round(r * Math.cos(theta[i])) + "px";
+        div.posY = Math.round(r * Math.sin(theta[i])) + "px";
+        div.style.top = circleHeight / 2 - parseInt(div.posY.slice(0, -2)) + "px";
+        div.style.left = circleHeight / 2 + parseInt(div.posX.slice(0, -2)) + "px";
 
-        clock.appendChild(dot);
+        clock.appendChild(div);
+        console.log(theta);
     }
+    theta = [];
 }
 
-generateTheta(60, 235, ".analogClock");
+generateTheta(60);
+setUpCircle(60, 235, ".analogClock", "dot");
+generateTheta(12);
+setUpCircle(12, 215, ".analogClock", "time");
